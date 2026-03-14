a89f416bd17530c4716d0be4ec48785f7d7d47ae
andreagropesquiza-blip <andre.agropesquiza@gmail.com>
2026-03-14 18:40:08 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.agroprecision.app',
+  appName: 'AgroPrecision',
+  webDir: 'dist'
+};
+
+export default config;
